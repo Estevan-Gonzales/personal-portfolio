8a69c8be3208a52fb9b37ebf47919be087feda93
Estevan <egonz94@gmail.com>
2024-01-24 17:56:10 -0600

    Added projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,16 +3,18 @@
 // Whenever we try to render an array containing JSX, React knows to render each JSX element separately
 function Project(props) {
     return (
-      <ul className="single-project-box">
+
+        <div className="row">
         {/* Here we map over each grocery item and return a new array of `li` elements that contains the grocery name */}
         {/* When using map you must provide a unique key attribute to each item. Ours is `item.id` */}
         {props.projects.map(item => (
-          <div className="single-project" key={item.title}>
+          <div className="bg-light border m-2 col-sm-10 col-md-5" key={item.title}>
             <a href={item.repo}><div>{item.title}</div></a>
-            <a href={item.url}><img className="project-image" src={item.image}></img></a>
+            <a href={item.url}><img src={item.image}></img></a>
           </div>
         ))}
-      </ul>
+        </div>
+
     );
   }
   
--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -3,8 +3,20 @@ import Project from "../Project";
 
 const projects = [
   {
+    title: 'Watchtogethr',
+    image: '/project-watchtogethr.png',
+    url: 'https://watchtogethr-aeeae66b60e6.herokuapp.com/Login',
+    repo: 'https://github.com/MikeTritsch/watchtogethr'
+  },
+  {
+    title: 'Dishr & Drinkr',
+    image: '/project-dishranddrinkr.png',
+    url: 'https://estevan-gonzales.github.io/project-1-dishr-and-drinkr/',
+    repo: 'https://github.com/Estevan-Gonzales/project-1-dishr-and-drinkr'
+  },
+  {
     title: 'Weather Dashboard',
-    image: "../public/project-weather.png",
+    image: "/project-weather.png",
     url: 'https://estevan-gonzales.github.io/module-6-challenge-weather-dashboard/',
     repo: 'https://github.com/Estevan-Gonzales/module-6-challenge-weather-dashboard'
   }
@@ -14,9 +26,9 @@ export default function Projects() {
   return (
     <div>
       <h1>Portfolio</h1>
-      <div>
+      <div className="container">
         <Project projects = {projects} />
-    </div>
+      </div>
     </div>
   );
 }
